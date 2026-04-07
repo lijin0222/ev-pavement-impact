28b47000803ff4ee9981e12631892a91a5652f21
Jin Li <56404054+lijin0222@users.noreply.github.com>
2026-04-07 19:50:11 +0800

    Update als_shifting_example_2.py

--- als-fitting-shifting/als_shifting_example_2.py
+++ als-fitting-shifting/als_shifting_example_2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import pandas as pd
 
 # File paths
@@ -31,24 +25,29 @@ for index, row in df.iterrows():
     fitted_mean_c5, fitted_std_dev_c5 = single_als_shift(mean=mean_c5,
                                                          std_dev=std_dev_c5,
                                                          num_samples=100000,
-                                                         x_shift=1000,
+                                                         x1_shift=2204.6,
+                                                         x2_shift=1000,
                                                          pen_rate=1)
     shifted_single_als_c5.append([shrp_id, fitted_mean_c5, fitted_std_dev_c5])
 
     # Step 2: Single axle shift for C9-sin
     print(f"\nSingle axle shift for C9-sin of SHRP_ID: {shrp_id}")
     mean_c9, std_dev_c9 = row['C9-sin-mu'], row['C9-sin-sigma']
+    weight1, weight2 = row['C9-tan-weight1'], row['C9-tan-weight2']
     fitted_mean_c9, fitted_std_dev_c9 = single_als_shift(mean=mean_c9,
                                                          std_dev=std_dev_c9,
                                                          num_samples=100000,
-                                                         x_shift=4409.2,
+                                                         weight1=weight1,
+                                                         weight2=weight2,
+                                                         x1_shift=4409.2,
+                                                         x2_shift=4409.2,
                                                          pen_rate=1)
     shifted_single_als_c9.append([shrp_id, fitted_mean_c9, fitted_std_dev_c9])
 
     # Step 3: Tandem axle shift for C9-tan
     print(f"\nTandem axle shift for C9-tan of SHRP_ID: {shrp_id}")
-    mean1, std_dev1, weight1 = row['C9-tan-mu1'], row['C9-tan-sigma1'], row['C9-tan-weight1']
-    mean2, std_dev2, weight2 = row['C9-tan-mu2'], row['C9-tan-sigma2'], row['C9-tan-weight2']
+    mean1, std_dev1= row['C9-tan-mu1'], row['C9-tan-sigma1']
+    mean2, std_dev2= row['C9-tan-mu2'], row['C9-tan-sigma2']
     (
         fitted_mean1,
         fitted_std_dev1,
@@ -64,8 +63,10 @@ for index, row in df.iterrows():
         std_dev2=std_dev2,
         weight2=weight2,
         num_samples=100000,
-        x_shift1=-3409.2,
-        x_shift2=1000,
+        x1_shift1=4409.2,
+        x1_shift2=0,
+        x2_shift1=1000,
+        x2_shift2=-3409.2,
         pen_rate=1
     )
     shifted_tandem_als.append([
@@ -108,4 +109,3 @@ df_shifted = df_shifted_c5.merge(df_shifted_c9, on='SHRP_ID').merge(df_shifted_t
 
 # Save the shifted parameters to a new Excel file
 df_shifted.to_excel(output_file, index=False)
-
